ec7fee774fb7df64b84e2a7c2f7af090b01e2f0b
Kévin Lejeune <lejeune-kevin@hotmail.fr>
2018-10-31 14:41:26 +0100

    commit to merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,42 @@
-backflip
+var home = angular.module('testApp', ['ngRoute']);
+
+home.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/home',{
+    templateUrl:'partials/home.html',
+    controller:'homeCtrl'
+  })
+  .when('/epicerie',{
+    templateUrl:'partials/epicerie.html',
+    controller: 'epicerieCtrl'
+  })
+  .when('/confiserie',{
+    templateUrl:'partials/confiserie.html',
+    controller: 'confiserieCtrl'
+  })
+  .when('/cremerie',{
+    templateUrl:'partials/cremerie.html',
+    controller: 'cremerieCtrl'
+  })
+  .otherwise({
+    redirectTo:'/home'
+  })
+}]);
+
+//on crée un controller rattaché à notre object "home"
+home.controller('homeCtrl', ['$scope', '$rootScope', function ($scope, $rootScope){
+$scope.sendData = function (){
+//crée une variable qui sera accessible dans tout le document
+    $rootScope.subjectList.push($scope.subject);
+    $rootScope.nameList.push($scope.name);
+    $rootScope.emailList.push($scope.email);
+    $rootScope.textList.push($scope.text);
+    }
+}]);
+home.controller('epicerieCtrl',['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $routeParams){
+    $scope.id = $routeParams.id;
+    $scope.subject = $rootScope.subjectList[$scope.id];
+    $scope.name = $rootScope.nameList[$scope.id];
+    $scope.email = $rootScope.emailList[$scope.id];
+    $scope.text = $rootScope.textList[$scope.id];
+}]);
