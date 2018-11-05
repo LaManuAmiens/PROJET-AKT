2900e33015222ea63be43a7f1c305cf18bff0092
Kévin Lejeune <lejeune-kevin@hotmail.fr>
2018-11-05 16:01:29 +0100

    THE FINAL COMMIT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var home = angular.module('testApp', ['ngRoute']);
 
+home.run(function($rootScope){
+  $rootScope.nameList;
+});
+
 home.config(['$routeProvider', function($routeProvider){
   $routeProvider
   .when('/home',{
@@ -27,24 +31,36 @@ home.config(['$routeProvider', function($routeProvider){
   })
 }]);
 
-home.controller("cremerieCtrl", function($scope, $http){
+home.controller("cremerieCtrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope){
   $http.get('assets/js/cremerie.json').then(function(res){
     $scope.products = res.data;
   });
-});
 
-home.controller("confiserieCtrl", function($scope, $http){
+  $scope.addToCart = function($scope, $rootScope) {
+    $rootScope.nameList.push($scope.name);
+  }
+}]);
+
+home.controller("confiserieCtrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope){
   $http.get('assets/js/confiserie.json').then(function(res){
     $scope.products = res.data;
   });
-});
 
-home.controller("epicerieCtrl", function($scope, $http){
+  $scope.addToCart = function($scope, $rootScope) {
+    $rootScope.nameList.push($scope.name);
+  }
+}]);
+
+home.controller("epicerieCtrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope){
   $http.get('assets/js/epicerie.json').then(function(res){
     $scope.products = res.data;
   });
 
-  $scope.addToCart = function($scope) {
-    console.log($scope);
+  $scope.addToCart = function($scope, $rootScope) {
+    $rootScope.nameList.push($scope.name);
   }
-});
+}]);
+
+home.controller("productsCtrl", ["$scope", "$rootScope", function($scope, $rootScope){
+  $scope.name = $rootScope.nameList;
+}]);
